0216d65155c559be70b30ecd9677297437db71db
Jimmy Miller <jimmyhmiller@gmail.com>
2019-01-17 13:49:28 -0500

    Clean up test playground file

--- api/test.js
+++ api/test.js
@@ -1,67 +1,13 @@
-const { buildMessage, buildPoll, buildOptions } = require('./util');
-const { vote, getVoteData, buildResponse } = require('./actions');
-require("./poll")
+const util = require('./util');
+const actions = require('./actions');
+const poll = require("./poll")
 
+// Playground file for testing. 
+// Ultimately, I have to change the exports to make things
+// actually testable. Not sure if there is a better approach
+
+
+// console.log in node is annoying, doesn't show all the data.
 console.log = (...args) => {
   console.dir(args, {depth: null})
 }
-
-
-const request = {
-  question: "What is is is?",
-  options: buildOptions(["Thing", "Stuff"]),
-  body: {
-    channel_id: "123"
-  }
-}
-
-const message = buildMessage({ 
-  question: "What is is is?",
-  options: buildOptions(["Thing", "Stuff"]),
-})
-
-
-const actionMessage1 = {
-  original_message: message,
-  "actions": [
-    {
-      "name": "1",
-      "type": "button",
-      "value": "1"
-    }
-  ],
-  user: {
-    name: "bob"
-  },
-  callback_id: "123"
-}
-
-const actionMessage2 = {
-  original_message: message,
-  "actions": [
-    {
-      "name": "0",
-      "type": "button",
-      "value": "0"
-    }
-  ],
-  user: {
-    name: "bob"
-  },
-  callback_id: "123"
-}
-
-const run = async () => {
-  try {
-    const updatedPoll1 = await vote(getVoteData(actionMessage1))
-    console.log(buildResponse(updatedPoll1))
-    const updatedPoll2 = await vote(getVoteData(actionMessage2))
-    console.log(buildResponse(updatedPoll2))
-
-    console.log(buildPoll(request))
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-run()
