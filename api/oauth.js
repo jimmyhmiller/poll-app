283ede4de2e4746f667dcbfe61ca5a0413fafc24
Jimmy Miller <jimmyhmiller@adzerk.com>
2019-04-08 13:10:24 -0400

    Fix typo

--- api/oauth.js
+++ api/oauth.js
@@ -33,7 +33,7 @@ const upsertUserAndTeamInfo = async ({ team_id, user_id, slack_access_token, acc
   }
 
   const { stripe_id } = await stripe.customers.create()
-  const response = await client.query(q.Update(teamInfo.ref, { data: { stripe_id: id }}))
+  const response = await client.query(q.Update(teamInfo.ref, { data: { stripe_id }}))
 
   return response.data;
 
